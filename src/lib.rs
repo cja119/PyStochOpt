c5c3ae00c6f2cb07c001baa1d9d5075b35fb56b1
Cameron Aldren <cja119@ic.ac.uk>
2024-11-16 12:49:43 +0000

    Beginning to implement the weather sampler - noticed that the current formulation isn't parallelised - need to addres this.

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,25 @@
+#![allow(non_snake_case)]
 use pyo3::prelude::*;
 use rayon::prelude::*;
 use pyo3::types::PyTuple;
-
+use csv::Reader;
 /// Formats the sum of two numbers as string.
+
 #[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
+#[pyo3(signature = (file_name, file_path=None))]
+fn read_csv(file_name: &str, file_path: Option<&str>) -> PyResult<String> {
+    let path = match file_path {
+        Some(path) => format!("{}/{}", path, file_name),
+        None => file_name.to_string(),
+    };
+    let mut rdr = Reader::from_path(path).map_err(|e| PyErr::new::<pyo3::exceptions::PyIOError, _>(format!("{}", e)))?;
+    let mut records = String::new();
+    for result in rdr.records() {
+        let record = result.map_err(|e| PyErr::new::<pyo3::exceptions::PyIOError, _>(format!("{}", e)))?;
+        records.push_str(&record.iter().map(|s| s.to_string()).collect::<Vec<String>>().join(","));
+        records.push_str("\n");
+    }
+    Ok(records)
 }
 
 #[pyclass]
@@ -71,8 +85,7 @@ impl StochasticGrid {
                 let grid_t:usize = (((t - delay) as f64 / grid_duration as f64).floor() as usize) * grid_duration as usize;
                 let grid_stage: usize = (grid_t as f64 / self.stage_duration as f64).floor() as usize;
                 let stage: usize = (t  as f64 / self.stage_duration as f64).floor() as usize;
-                let scenario: usize = self.n_scenarios.pow(stage as u32);
-                let grid_scenario: usize = self.n_scenarios.pow(grid_stage as u32);      
+                let scenario: usize = self.n_scenarios.pow(stage as u32);   
                 (0..scenario).into_iter().for_each(|s| {
                     let key: usize = scenario * (t - stage * self.stage_duration) + s + self.stage_duration * (scenario -1) / (self.n_scenarios - 1);
                     let ratio: usize = self.n_stages.pow((stage - grid_stage) as u32);
@@ -94,5 +107,6 @@ impl StochasticGrid {
 #[pymodule]
 fn PyStochOpt(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_class::<StochasticGrid>()?;
+    m.add_function(wrap_pyfunction!(read_csv, m)?)?;
     Ok(())
 }
