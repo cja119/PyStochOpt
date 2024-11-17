60a7f8b8c08f60d8d7cfb9bb62cae8ac6f19397b
Cameron Aldren <cja119@ic.ac.uk>
2024-11-17 14:14:09 +0000

    Data loader and sampler appears to be working. Now onto checking the contiguity of the datasets

--- src/lib.rs
+++ src/lib.rs
@@ -3,23 +3,32 @@ use pyo3::prelude::*;
 use rayon::prelude::*;
 use pyo3::types::PyTuple;
 use csv::Reader;
+use rand::Rng;
 /// Formats the sum of two numbers as string.
 
-#[pyfunction]
-#[pyo3(signature = (file_name, file_path=None))]
-fn read_csv(file_name: &str, file_path: Option<&str>) -> PyResult<String> {
+
+fn read_csv(file_name: &str, file_path: Option<&str>) -> Vec<(usize, f64)> {
     let path = match file_path {
         Some(path) => format!("{}/{}", path, file_name),
         None => file_name.to_string(),
     };
-    let mut rdr = Reader::from_path(path).map_err(|e| PyErr::new::<pyo3::exceptions::PyIOError, _>(format!("{}", e)))?;
-    let mut records = String::new();
-    for result in rdr.records() {
-        let record = result.map_err(|e| PyErr::new::<pyo3::exceptions::PyIOError, _>(format!("{}", e)))?;
-        records.push_str(&record.iter().map(|s| s.to_string()).collect::<Vec<String>>().join(","));
-        records.push_str("\n");
+    let mut rdr = Reader::from_path(path).expect("Failed to open CSV file");
+    let mut rows: Vec<(usize, f64)> = Vec::new();
+    for (i, result) in rdr.records().enumerate() {
+        if i == 0 {
+            continue; // Skip the first line
+        }
+        let mut record = result.expect("Failed to read CSV record");
+        if record.iter().any(|field| field.contains(' ')) {
+            record = csv::StringRecord::from(record.iter().flat_map(|field| field.split_whitespace()).collect::<Vec<&str>>());
+        }
+        let row: (usize, f64) = (
+            record[0].parse().unwrap(),
+            record[1].parse().unwrap()
+        );
+        rows.push(row);
     }
-    Ok(records)
+    rows
 }
 
 #[pyclass]
@@ -34,13 +43,15 @@ fn build_grid(n_stages: usize, n_scenarios: usize,stage_duration: usize) ->  Vec
     let total_time: usize = stage_duration * (n_scenarios.pow(n_stages as u32+ 1) - 1) / (n_scenarios - 1);
     let mut grid: Vec<(usize,usize)>   = vec![(0,0); total_time];
 
-    (0..((n_stages+1) * stage_duration)).into_iter().for_each(|t| {
+    (0..((n_stages+1) * stage_duration)).into_par_iter().map(|t| {
         let stage: usize = (t as f64 / stage_duration as f64).floor() as usize;
         let scenario: usize = n_scenarios.pow(stage as u32);
-        (0..scenario).into_iter().for_each(|s| {
+        (0..scenario).map(|s| {
             let key: usize = scenario * (t - stage * stage_duration) + s + stage_duration * (scenario -1) / (n_scenarios - 1);
-            grid[key] = (s, t);
-        });
+            (key, (s, t))
+        }).collect::<Vec<_>>()
+    }).flatten().collect::<Vec<_>>().into_iter().for_each(|(key, value)| {
+        grid[key] = value;
     });
     return grid;
 }   
@@ -68,45 +79,78 @@ impl StochasticGrid {
         let total_time: usize = self.stage_duration * (self.n_scenarios.pow(self.n_stages as u32+ 1) - 1) / (self.n_scenarios - 1);
         let delay: usize = delay.unwrap_or(0);
         let mut new_grid: Vec<(usize,usize)>   = vec![(0,0); total_time];
-    
-        (0..((self.n_stages+1) * self.stage_duration)).into_iter().for_each(|t| {
+        (0..((self.n_stages+1) * self.stage_duration)).into_par_iter().map(|t| {
             if t < delay {     
                 let grid_t:usize = ((t as f64 / grid_duration as f64).floor() as usize) * grid_duration as usize;
                 let grid_stage: usize = (grid_t as f64 / self.stage_duration as f64).floor() as usize;
                 let stage: usize = (t as f64 / self.stage_duration as f64).floor() as usize;
-                let scenario: usize = self.n_scenarios.pow(stage as u32);      
-                (0..scenario).into_iter().for_each(|s| {
+                let scenario: usize = self.n_scenarios.pow(stage as u32);  
+                (0..scenario).map(|s| {
                     let key: usize = scenario * (t - stage * self.stage_duration) + s + self.stage_duration * (scenario -1) / (self.n_scenarios - 1);
-                    let ratio: usize = self.n_stages.pow((stage - grid_stage) as u32);
-                    new_grid[key] = ((s as f64/ratio as f64).floor() as usize, 0);
-                }); 
-            }
-            else {     
-                let grid_t:usize = (((t - delay) as f64 / grid_duration as f64).floor() as usize) * grid_duration as usize;
-                let grid_stage: usize = (grid_t as f64 / self.stage_duration as f64).floor() as usize;
-                let stage: usize = (t  as f64 / self.stage_duration as f64).floor() as usize;
-                let scenario: usize = self.n_scenarios.pow(stage as u32);   
-                (0..scenario).into_iter().for_each(|s| {
-                    let key: usize = scenario * (t - stage * self.stage_duration) + s + self.stage_duration * (scenario -1) / (self.n_scenarios - 1);
-                    let ratio: usize = self.n_stages.pow((stage - grid_stage) as u32);
-                    for i in 0..ratio {
-                        new_grid[key + i] = ((s as f64/ratio as f64).floor() as usize, grid_t);
+                    let n_stages = self.n_stages;
+                    let ratio: usize = n_stages.pow((stage - grid_stage) as u32);
+                    (key, ((s as f64/ratio as f64).floor() as usize, 0))
+                    }).collect::<Vec<_>>()
+                } 
+                else {     
+                    let grid_t:usize = (((t - delay) as f64 / grid_duration as f64).floor() as usize) * grid_duration as usize;
+                    let grid_stage: usize = (grid_t as f64 / self.stage_duration as f64).floor() as usize;
+                    let stage: usize = (t  as f64 / self.stage_duration as f64).floor() as usize;
+                    let scenario: usize = self.n_scenarios.pow(stage as u32);  
+                    (0..scenario).map(|s| {
+                        let key: usize = scenario * (t - stage * self.stage_duration) + s + self.stage_duration * (scenario -1) / (self.n_scenarios - 1);
+                        let ratio: usize = self.n_stages.pow((stage - grid_stage) as u32);
+                        (key, ((s as f64/ratio as f64).floor() as usize, grid_t))
+                        }).collect::<Vec<_>>()
                     }
-                    //new_grid[key] = ((s as f64/ratio as f64).floor() as usize, grid_t);
-                }); 
+            }).flatten().collect::<Vec<_>>().into_iter().for_each(|(key, value)| {
+                new_grid[key] = value;
+            }); 
+            return new_grid;
+        }
+        #[pyo3(signature = (file_name, file_path=None))]
+        fn add_dataset(&mut self, file_name: &str, file_path: Option<&str>) -> Vec<(usize, f64)> {
+            let dataset: Vec<(usize, f64)> = read_csv(file_name, file_path);
+            let total_time: usize = self.stage_duration * (self.n_scenarios.pow(self.n_stages as u32+ 1) - 1) / (self.n_scenarios - 1);
+            let mut samples: Vec<(usize,f64)>   = vec![(0,0.0); total_time];
+            let mut start_points: Vec<usize> = vec![0; self.n_scenarios.pow(self.n_stages as u32)];
+
+            for stage in 0..self.n_stages {
+                if stage != 0 {
+                    for s in self.n_scenarios.pow((stage - 1) as u32)..self.n_scenarios.pow(stage as u32) {
+                        let n_branch: usize = self.n_stages + 1 - (s as f64 + 1.0).log(self.n_scenarios as f64).ceil() as usize;
+                        let n_samp: usize = (n_branch * (n_branch + 1) * (n_branch + 2) / 6) * self.stage_duration;
+                        let startpoint = rand::thread_rng().gen_range(0..dataset.len() - n_samp);
+                        start_points[s] = startpoint;
+                }}
+                else {
+                    let s = stage;
+                    let n_branch: usize = self.n_stages + 1;
+                    let n_samp: usize = (n_branch * (n_branch + 1) * (n_branch + 2) / 6) * self.stage_duration;
+                    let startpoint = rand::thread_rng().gen_range(0..dataset.len() - n_samp);
+                    start_points[s] = startpoint;
+                }
             }
-        });
-        return new_grid;
-    }
-    
 
+
+            for s in 0..self.n_scenarios.pow(self.n_stages as u32) {
+                println!("Scenario: {}, Start_time: {}, End_time {}", s,self.stage_duration * (s as f64 + 1.0).log(self.n_scenarios as f64).ceil() as usize,self.stage_duration * (self.n_stages + 1));
+                for t in self.stage_duration * (s as f64 + 1.0).log(self.n_scenarios as f64).ceil() as usize..self.stage_duration * (self.n_stages + 1) as usize {
+                    let stage: usize = (t as f64 / self.stage_duration as f64).floor() as usize;
+                    let scenario: usize = self.n_scenarios.pow(stage as u32);  
+                    let key: usize = scenario * (t - stage * self.stage_duration) + s + self.stage_duration * (scenario -1) / (self.n_scenarios - 1);
+                    
+                    samples[key] = (s, dataset[start_points[s] + t - self.stage_duration * (s as f64 + 1.0).log(self.n_scenarios as f64).ceil()  as usize].1);
+                }
+            }
+
+            return samples;
+        }
 }
-
 
 /// A Python module implemented in Rust.
 #[pymodule]
 fn PyStochOpt(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_class::<StochasticGrid>()?;
-    m.add_function(wrap_pyfunction!(read_csv, m)?)?;
     Ok(())
 }
